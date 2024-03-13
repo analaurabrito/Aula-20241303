f64f1eb42cb185ad64c6fd382d5d827f539239aa
Aura <111578898+analaurabrito@users.noreply.github.com>
2024-03-13 08:43:52 -0300

    Add files via upload

---
+++ Aula 20241303/Aula 20241303/Disciplina.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20241303
+{
+    internal class Disciplina
+    {
+        private string codigo { get; set; }
+        private string nome { get; set; }
+        private Professor professor { get; set;}
+        private List<string> preRequisitos { get; set; }
+
+
+        public Disciplina(string codigo, string nome)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+        }
+
+        public void addPreRequisito(Disciplina d)
+        {
+            preRequisitos.Add(d.codigo);
+        }
+    }
+}
---
+++ Aula 20241303/Aula 20241303/Professor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_20241303
+{
+    internal class Professor
+    {
+        private string ID { get; set; }
+        private string nome { get; set; }
+
+        public Professor(string iD, string nome)
+        {
+            ID = iD;
+            this.nome = nome;
+        }
+    }
+}
